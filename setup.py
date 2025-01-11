aa2a8cee1a2b5ed63f3ac24df9a688cf283d1274
Sunny Sinha <sunnysinha@Sunnys-MacBook-Air.local>
2025-01-12 01:53:54 +0530

    t

--- setup.py
+++ setup.py
@@ -4,6 +4,25 @@ setup(
     name='vm_tool',
     version='0.1',
     packages=find_packages(),
+    description='''
+    This is a tool to setup VMs using Ansible.
+
+    Example usage:
+
+    from vm_tool import SetupRunner
+
+    runner = SetupRunner(
+        ssh_host='your_host', # e.g. 192.168.1.1
+        ssh_user='your_user', # e.g. root
+        ssh_password='your_password', # e.g. password
+        become_pass='your_become_pass', # e.g. password
+        github_username='your_github_username', # e.g. username
+        github_token='your_github_token', # e.g. token
+        github_project_url='your_github_project_url' # e.g. https://github.com/username/repo
+    )
+
+    runner.run_setup()
+    ''',
     install_requires=[
         'ansible',
         'ansible-runner',
