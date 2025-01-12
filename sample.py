96562abd5ee9470f16406c9128094e6797ff20b4
Sunny Sinha <thesunnysinha@gmail.com>
2025-01-12 11:17:46 +0000

    code clean up

--- sample.py
+++
@@ -1,9 +0,0 @@
-from vm_tool.runner import SetupRunner
-
-runner = SetupRunner(
-    github_username='your_github_username', # e.g. username
-    github_token='your_github_token', # e.g. token
-    github_project_url='your_github_project_url' # e.g. https://github.com/username/repo
-)
-
-runner.run_setup()
