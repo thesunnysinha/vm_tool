5818307c57b1f8ae19b46e39154c0ce62fadcdf3
Sunny Sinha <sunnysinha@Sunnys-MacBook-Air.local>
2025-01-12 14:01:03 +0530

    versoning through bumpversion fixed

--- codePushToGithub.sh
+++ codePushToGithub.sh
@@ -12,7 +12,7 @@ read -p "Enter the branch name: " branch_name
 # Check if the branch name is "main"
 if [ "$branch_name" = "main" ]; then
 
-    bump2version patch
+    bump2version patch --allow-dirty
 
     # Add all changes
     git add .
