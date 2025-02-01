b5ff3cdd274366f06bd552063bafb8b933688a14
Sunny Kumar Sinha <thesunnysinha@gmail.com>
2025-02-02 01:35:27 +0530

    Bump version: 1.0.21 → 1.0.22

--- vm_tool/cli.py
+++ vm_tool/cli.py
@@ -2,7 +2,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Setup VMs using Ansible.')
-    parser.add_argument('--version', action='version', version='1.0.21')
+    parser.add_argument('--version', action='version', version='1.0.22')
 
     args = parser.parse_args()
 
